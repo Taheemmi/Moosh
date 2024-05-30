4ed83db41c43afdb680f373a8c5867e4df6991aa
Taheemmi <135760363+Taheemmi@users.noreply.github.com>
2024-05-30 09:35:26 +0100

    Update App.js

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -2,14 +2,17 @@
 
 import React, { useEffect, useRef, useState } from 'react';
 import Game from './game/base';
-import OptionsPage from './game/OptionsPage'; // Import the OptionsPage component
+import Settings from './game/Settings'; // Import the Settings component
+import Barn from './game/Barn'; // Import barn
 import './index.css'; // change if CSS has been relocated
 
 
 function App() {
   const canvasRef = useRef(null);
   const [showMenu, setShowMenu] = useState(true);
-  const [showOptions, setShowOptions] = useState(false); // Add state for showing options
+  const [showOptions, setShowOptions] = useState(false); 
+  const [showBarn, setShowBarn] = useState(false); // added state for showing barn 
+
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -44,35 +47,54 @@ function App() {
     setShowMenu(false); // Hide the main menu when options are shown
   };
 
+  const handleBarnClick = () => {
+    setShowBarn(true);
+    setShowMenu(false);
+  }
+
   //function to go back a notch
   const handleBackClick = () => {
     setShowOptions(false); // hide the options page
+    setShowBarn(false); // hide the barn
     setShowMenu(true); // show the main menu
   };
 
   return (
-    <div className="App">
+
+    <div className={`App ${!showMenu ? 'background-hidden' : ''}`}>
+      <div className="background-container">
+        <div className="background-image"></div>
+      </div>
       {showMenu ? (
         <div className='centre-menu'>
 
           <h3>Main Menu</h3>
           <button className='menu-buttons' onClick={handleStartClick}>Start Game</button>
-          <button className='menu-buttons' onClick={handleStartClick}>Barn</button>
+          <button className='menu-buttons' onClick={handleBarnClick}>Barn</button>
           <button className='menu-buttons' onClick={handleSettingsClick}>Settings</button> {/* Call handleSettingsClick for "Settings" button */}
         </div>
-        
-
       ) : null}
 
       {showOptions ? (
         <div>
-          <OptionsPage onBackClick={handleBackClick} /> 
+          <Settings onBackClick={handleBackClick} /> 
         </div>
       ) : null}
 
+      {showBarn ? (
+        <div>
+          <Barn onBackClick={handleBackClick} />
+        </div>
+      ) : null}
+
+      {!showMenu && (
+
+      <div>
       <canvas ref={canvasRef} style={{ border: '1px solid black', display: showMenu ? 'none' : 'block' }} />
+      <button className='menu-buttons' onClick={handleBackClick} style={{ position: 'absolute', top: 20, left: 20}}>Return to Menu</button>
     </div>
+  )}
+  </div>
   );
 }
-
 export default App;
